6766e6c268c008a88f0cd456a41ba3e31b86bcec
bahromasrorkulov <bahrom142214@gmail.com>
2022-11-14 21:47:19 +0500

    1-commit

---
+++ src/main/java/uz/minustelegramapplication/MinusTelegramApplication.java
@@ -0,0 +1,13 @@
+package uz.minustelegramapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinusTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinusTelegramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/minustelegramapplication/MinusTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.minustelegramapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinusTelegramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
