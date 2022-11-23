def679e33b69280eb310b769a5ceb2528e6c904c
bahromasrorkulov <bahrom142214@gmail.com>
2022-11-23 08:36:43 +0500

    DashboardController fixed

--- src/main/java/uz/minustelegramapplication/service/impl/DashboardServiceImpl.java
+++ src/main/java/uz/minustelegramapplication/service/impl/DashboardServiceImpl.java
@@ -3,25 +3,42 @@ package uz.minustelegramapplication.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import uz.minustelegramapplication.entity.Channel;
+import uz.minustelegramapplication.entity.Group;
+import uz.minustelegramapplication.repo.ChannelRepository;
+import uz.minustelegramapplication.repo.GroupRepository;
 import uz.minustelegramapplication.response.ResponseData;
 import uz.minustelegramapplication.service.ChannelService;
 import uz.minustelegramapplication.service.DashboardService;
 import uz.minustelegramapplication.service.GroupService;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class DashboardServiceImpl implements DashboardService {
 
+    private final ChannelRepository channelRepository;
     private final ChannelService service;
-
+    private final GroupRepository groupRepository;
     private final GroupService groupService;
+
     @Override
-    public ResponseEntity<ResponseData<Integer>> getCount(Integer user_id) {
-        return ResponseData.success200(service.getCount(user_id));
+    public ResponseEntity<ResponseData<Integer>> getChannel(Integer id) {
+        Optional<Channel> channel = channelRepository.findById(id);
+        if (channel.isEmpty()) {
+            return ResponseData.notFoundData("Channel is not found !!!");
+        }
+        return ResponseData.success200(service.getCount(id));
     }
 
     @Override
-    public ResponseEntity<ResponseData<Integer>> getGroup(Integer user_id) {
-        return ResponseData.success200(groupService.getCountGroup(user_id));
+    public ResponseEntity<ResponseData<Integer>> getGroup(Integer id) {
+        Optional<Group> group = groupRepository.findById(id);
+        if (group.isEmpty()) {
+            return ResponseData.notFoundData("Group is not found !!!");
+        }
+
+        return ResponseData.success200(groupService.getCountGroup(id));
     }
 }
